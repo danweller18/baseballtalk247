462207807396099c2602879ed7eec052576c612e
Daniel Weller <ravensfan55222@aol.com>
2013-12-10 10:59:08 -0500

    Update user without password working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Baseballtalk247::Application.routes.draw do
   devise_for :admins
-  devise_for :users
-  root 'static_pages#home'
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  root 'static_pages#index'
+  match '/home', 	to: 'static_pages#home',	via: 'get'
   match '/contact',	to: 'static_pages#contact', 	via: 'get'
   match '/about',	to: 'static_pages#about',	via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
