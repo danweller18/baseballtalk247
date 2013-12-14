c0510d2c8f2a5249139c1a29ced1a79afb0991e2
Daniel Weller <ravensfan55222@aol.com>
2013-12-14 11:42:18 -0500

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:signin, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation, :current_password)}
-      devise_parameter_sanitizer.for(:sign_up) { |a| a.permit(:username,) }
+#      devise_parameter_sanitizer.for(:sign_up) { |a| a.permit(:username,) }
       devise_parameter_sanitizer.for(:sign_in) { |a| a.permit(:signin, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) {|a| a.permit(:username, :email, :password, :password_confirmation, :current_password)}
   end
