596822da6c6fe02c9956970d72832ee3eb414851
Daniel Weller <ravensfan55222@aol.com>
2013-12-14 17:57:16 -0500

    update mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Baseballtalk247::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Development options for the mailler
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
