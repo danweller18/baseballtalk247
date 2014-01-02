58429bed28a3f642d4777a3adbc2efad8a87bcf0
Daniel Weller <ravensfan55222@aol.com>
2014-01-02 01:26:34 -0500

    update file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Baseballtalk247::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
+   
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -79,7 +80,7 @@ Baseballtalk247::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Production Mailer
-  config.action_mailer_default_url_options = { :host => 'http://baseballtalk247.herokuapp.com' }
+  config.action_mailer_default_url_options = { :host => 'baseballtalk247.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
