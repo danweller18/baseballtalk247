c47d17c9f332bccd0d5c64a791e429a092075406
Daniel Weller <ravensfan55222@aol.com>
2014-01-06 13:59:11 -0500

    update files

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :bats
   default_scope -> { order('created_at DESC') }
   validates :bats_id, presence: true
-  validates :pros, presence: true, length: { maximum: 200 }
-  validates :cons, presence: true, length: { maximum: 200 }
+  validates :pros, presence: true
+  validates :cons, presence: true
   validates :user_id, presence: true
 end
