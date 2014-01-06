ba1d192a0ec7c68da2759c2e30e1f0cb9efd7a7a
Daniel Weller <ravensfan55222@aol.com>
2014-01-06 09:18:15 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231201012) do
+ActiveRecord::Schema.define(version: 20140103050820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20131231201012) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "bats_id"
   end
 
   add_index "reviews", ["user_id", "created_at"], name: "index_reviews_on_user_id_and_created_at", using: :btree
